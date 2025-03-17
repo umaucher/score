972c0933951560a34b09e4ab682de86efa84aac9
Maximilian Sören Pollak <maximilian.pollak@expleogroup.com>
2025-03-17 17:36:42 +0100

    docs: Grouped incremental & live_preview (#700)
    Grouping incremental & live-_preview scripts due to many similarities.
    Extended docs.bzl functionality.

--- docs/_tooling/docs.bzl
+++ docs/_tooling/docs.bzl
@@ -44,7 +44,7 @@ load("@rules_pkg//pkg:mappings.bzl", "pkg_files")
 load("@rules_pkg//pkg:tar.bzl", "pkg_tar")
 load("@rules_python//python:pip.bzl", "compile_pip_requirements")
 load("@rules_python//sphinxdocs:sphinx.bzl", "sphinx_build_binary", "sphinx_docs")
-load("//docs:_tooling/extensions/score_source_code_linker/collect_source_files.bzl", "collect_source_files_for_score_source_code_linker")
+load("//docs:_tooling/extensions/score_source_code_linker/collect_source_files.bzl", "parse_source_files_for_needs_links")
 load("//tools/dash:dash.bzl", "dash_license_checker")
 load("//tools/testing/pytest:defs.bzl", "score_py_pytest")
 
@@ -53,23 +53,29 @@ sphinx_requirements = all_requirements + [
     ":plantuml_for_python",
 ]
 
-def docs():
+def docs(source_files_to_scan_for_needs_links = None, source_dir = "docs", conf_dir = "docs", build_dir_for_incremental = "_build"):
     """
     Creates all targets related to documentation.
     By using this function, you'll get any and all updates for documentation targets in one place.
     Current restrictions:
     * only callable from 'docs/BUILD'
     """
-    collect_source_files_for_score_source_code_linker(
-        name = "collected_files_for_score_source_code_linker",
-        deps = [
-            ":score_metamodel",
-            ":score_source_code_linker",
-        ],
+
+    # Parse source files for needs links
+    parse_source_files_for_needs_links(
+        name = "score_source_code_parser",
+        srcs_and_deps = source_files_to_scan_for_needs_links if source_files_to_scan_for_needs_links else [],
     )
 
+    # Get the output of source_code_linker
+    # Does not work:
+    # rule_info = native.existing_rule(source_code_linker.name)
+    # source_code_links = rule_info[SourceCodeLinks].file.path
+    # Workaround:
+    source_code_links = "score_source_code_parser.json"
+
     # Run-time build of documentation, incl. incremental build support and non-IDE live preview.
-    _incremental()
+    _incremental(":score_source_code_parser", source_code_links, source_dir = source_dir, conf_dir = conf_dir, build_dir = build_dir_for_incremental)
 
     # Virtual python environment for working on the documentation (esbonio).
     # incl. python support when working on conf.py and sphinx extensions.
@@ -79,22 +85,45 @@ def docs():
     # creates :docs target for build time documentation
     _docs()
 
-def _incremental():
+def _incremental(source_code_linker, source_code_links, source_dir = "docs", conf_dir = "docs", build_dir = "_build", extra_dependencies = list()):
     """
     A target for building docs incrementally at runtime, incl live preview.
+    Args:
+        source_code_linker: The source code linker target to be used for linking source code to documentation.
+        source_code_links: The output from the source code linker.
+        source_dir: Directory containing the source files for documentation.
+        conf_dir: Directory containing the Sphinx configuration.
+        build_dir: Directory to output the built documentation.
+        extra_dependencies: Additional dependencies besides the centrally maintained "sphinx_requirements".
     """
+
+    dependencies = sphinx_requirements + extra_dependencies
 
     py_binary(
         name = "incremental",
-        srcs = ["_tooling/incremental.py"],
-        data = [":collected_files_for_score_source_code_linker"],
-        deps = sphinx_requirements,
+        srcs = ["//docs:_tooling/incremental.py"],
+        data = [source_code_linker, "//docs:docs_assets"],
+        deps = dependencies,
+        env = {
+            "SOURCE_CODE_LINKS": source_code_links,
+            "SOURCE_DIRECTORY": source_dir,
+            "CONF_DIRECTORY": conf_dir,
+            "BUILD_DIRECTORY": build_dir,
+            "ACTION": "incremental",
+        },
     )
 
     py_binary(
         name = "live_preview",
-        srcs = ["_tooling/live_preview.py"],
-        deps = sphinx_requirements,
+        srcs = ["//docs:_tooling/incremental.py"],
+        data = ["//docs:docs_assets"],
+        deps = dependencies,
+        env = {
+            "SOURCE_DIRECTORY": source_dir,
+            "CONF_DIRECTORY": conf_dir,
+            "BUILD_DIRECTORY": build_dir,
+            "ACTION": "live_preview",
+        },
     )
 
 def _ide_support():
@@ -126,7 +155,7 @@ def _docs():
             "-W",
             "--keep-going",
             # This is 'overwriting' the configuration parameter inside sphinx. As we only get this information during runtime
-            "--define=source_code_linker_file=$(location :collected_files_for_score_source_code_linker)",
+            "--define=source_code_linker_file=$(location :score_source_code_parser)",
         ],
         formats = [
             "html",
@@ -136,7 +165,7 @@ def _docs():
             "manual",
         ],
         tools = [
-            ":collected_files_for_score_source_code_linker",
+            ":score_source_code_parser",
             ":plantuml",
         ],
     )
--- docs/_tooling/incremental.py
+++ docs/_tooling/incremental.py
@@ -12,12 +12,18 @@
 # *******************************************************************************
 
 import os
-import subprocess
 import argparse
 import debugpy
+import logging
+import sys
 
+from pathlib import Path
+from python.runfiles import Runfiles
 from sphinx.cmd.build import main as sphinx_main
+from sphinx_autobuild.__main__ import main as sphinx_autobuild_main
 
+
+logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -27,56 +33,98 @@ parser.add_argument("--debug", help="Enable Debugging via debugpy", action="stor
 args = parser.parse_args()
 if args.debug:
     debugpy.listen(("0.0.0.0", args.debug_port))
-    print("Waiting for client to connect on port: " + str(args.debug_port))
+    logger.info("Waiting for client to connect on port: " + str(args.debug_port))
     debugpy.wait_for_client()
     pass
 
-# sphinx will print relative paths to the current directory.
-# Change to the workspace root so that the paths are readable and clickable.
+
+def get_env(name):
+    val = os.environ.get(name, None)
+    logger.debug(f"Env: {name} = {val}")
+    if val is None:
+        raise ValueError(f"Environment variable {name} is not set")
+    return val
+
+
+def get_runfiles_dir() -> Path:
+    if r := Runfiles.Create():
+        # Runfiles are only available when running in Bazel.
+        # bazel build and bazel run are both supported.
+        # i.e. `bazel build //docs:docs` and `bazel run //docs:incremental`.
+        logger.info("Using runfiles to determine plantuml path.")
+
+        runfiles_dir = Path(r.EnvVars()["RUNFILES_DIR"])
+
+        if not runfiles_dir.exists():
+            sys.exit(
+                f"Could not find runfiles at {runfiles_dir}. Have a look at "
+                "README.md for instructions on how to build docs."
+            )
+
+    else:
+        # The only way to land here is when running from within the virtual
+        # environment created by the `docs:ide_support` rule in the BUILD file.
+        # i.e. esbonio or manual sphinx-build execution within the virtual
+        # environment.
+        # We'll still use the plantuml binary from the bazel build.
+        # But we need to find it first.
+        logger.info("Running outside bazel.")
+
+        git_root = Path(__file__).resolve().parents[3]
+        assert (
+            git_root / ".git"
+        ).exists(), f"Could not find git root. Assumed path: {git_root}"
+
+        runfiles_dir = git_root / "bazel-bin" / "docs" / "ide_support.runfiles"
+        if not runfiles_dir.exists():
+            sys.exit(
+                f"Could not find ide_support.runfiles at {runfiles_dir}. "
+                "Have a look at README.md for instructions on how to build docs."
+            )
+
+    return runfiles_dir
+
+
+# Registering a default value
+source_code_linker_file = ""
+
+runfiles_dir = get_runfiles_dir()
+# runfiles_dir points to a cache directory which has a new hash every time.
+# Use the relative path that is available from workspace root.
+relative_path = Path("bazel-out") / str(runfiles_dir).split("/bazel-out/", 1)[-1]
+
+
+# Asset_dir is interpreted by sphinx. Paths are relative conf.py (conf_dir)
+assets_dir_prefix = str(Path("..") / relative_path) + "/" + "_main/docs"
+
+
 workspace = os.getenv("BUILD_WORKSPACE_DIRECTORY")
-
-# Initialize with a default value
-output_files = ""
-
 if workspace:
-    # This will gives us all 'output files' and their location that are required
-    #  by the 'source_link' extensions
-    subprocess.run(
-        [
-            "bazel",
-            "build",
-            "--noremote_accept_cached",
-            "//docs:collected_files_for_score_source_code_linker",
-        ],
-        cwd=workspace,
-    )
-
-    process = subprocess.Popen(
-        [
-            "bazel",
-            "cquery",
-            "//docs:collected_files_for_score_source_code_linker",
-            "--output=files",
-        ],
-        cwd=workspace,
-        stdout=subprocess.PIPE,
-    )
-
-    output_files = process.stdout.readline().decode().strip() if process.stdout else ""
-
     os.chdir(workspace)
 
-sphinx_main(
-    [
-        "docs",  # src dir
-        "_build",  # out dir
-        "-W",  # treat warning as errors
-        "--keep-going",  # do not abort after one error
-        "-T",  # show details in case of errors in extensions
-        "--jobs",
-        "auto",
-        "--conf-dir",
-        "docs",
-        f"--define=source_code_linker_file={output_files}",
+
+base_arguments = [
+    get_env("SOURCE_DIRECTORY"),
+    get_env("BUILD_DIRECTORY"),
+    "-W",  # treat warning as errors
+    "--keep-going",  # do not abort after one error
+    "-T",  # show details in case of errors in extensions
+    "--jobs",
+    "auto",
+    "--conf-dir",
+    get_env("CONF_DIRECTORY"),
+]
+
+
+action = get_env("ACTION")
+if action == "live_preview":
+    sphinx_autobuild_main(base_arguments)
+else:
+    filename = get_env("SOURCE_CODE_LINKS")
+    source_code_linker_file = str(relative_path.parent) + "/" + filename
+    incremental_args = base_arguments + [
+        # Overwriting config values
+        f"--define=source_code_linker_file={source_code_linker_file}",
+        f"--define=html_static_path={assets_dir_prefix}/_assets,{assets_dir_prefix}/_tooling/assets",
     ]
-)
+    sphinx_main(incremental_args)
